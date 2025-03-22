e1a39615ae9319e42dad569e00b56670fdbdd723
Betül Oran <116026974+betuloran@users.noreply.github.com>
2025-03-22 12:59:11 +0300

    css properties added

--- jira-app/src/App.js
+++ jira-app/src/App.js
@@ -4,25 +4,25 @@ import TaskList from './components/TaskList';
 import { useState } from 'react';
 
 function App() {
-  const [tasks, setTask] = useState([]);
+  const [tasks, setTasks] = useState([]);
   const createTask = (title, taskDesc) => {
-    const createdTask = [
+    const createdTasks = [
       ...tasks,
       {
         id: Math.round(Math.random() * 999999),
         title,
         taskDesc,
       },
-   ];
-  setTask(createdTask);
-}
-return (
-  <div className="App">
-    <TaskCreate onCreate={createTask} />
-    <h1>Görevler</h1>
-    <TaskList tasks={tasks} />
-  </div>
-);
+    ];
+    setTasks(createdTasks);
+  };
+  return (
+    <div className="App">
+      <TaskCreate onCreate={createTask} />
+      <h1>Görevler</h1>
+      <TaskList tasks={tasks} />
+    </div>
+  );
 }
 
 export default App;
--- jira-app/src/components/TaskShow.js
+++ jira-app/src/components/TaskShow.js
@@ -1,17 +1,17 @@
 function TaskShow({ task }) {
-    return <div className="tasks">
-        <div className="task-show" >
-          <h3 className="task-title">Göreviniz</h3>
-        <p>{task.title}</p>
+    console.log(task);
+    return (
+        <div className="task-show">
+            <h3 className="task-title">Göreviniz</h3>
+            <p>{task.title}</p>
             <h3 className="task-title">Yapılacaklar</h3>
-        <p>{task.taskDesc}</p>
-        <div>
+            <p>{task.taskDesc}</p>
+            <div>
                 <button className="task-delete">Sil</button>
-                <button className="task-update" >Güncelle</button>
-        </div>  
+                <button className="task-edit">Güncelle</button>
+            </div>
         </div>
-        
-    </div> ;
+    );
 }
 
 export default TaskShow;
