89d883ac801db4e62d1f6eb3de96bc4642955b80
Betül Oran <116026974+betuloran@users.noreply.github.com>
2025-03-22 12:55:52 +0300

    button css properties added

--- jira-app/src/components/TaskShow.js
+++ jira-app/src/components/TaskShow.js
@@ -1,6 +1,15 @@
-function TaskShow({task}) {
-    return <div
-    >
+function TaskShow({ task }) {
+    return <div className="tasks">
+        <div className="task-show" >
+          <h3 className="task-title">Göreviniz</h3>
+        <p>{task.title}</p>
+            <h3 className="task-title">Yapılacaklar</h3>
+        <p>{task.taskDesc}</p>
+        <div>
+                <button className="task-delete">Sil</button>
+                <button className="task-update" >Güncelle</button>
+        </div>  
+        </div>
         
     </div> ;
 }
